331b95036bfafe8f4a4b951eaac60f7e29f0a591
KongWiki <kongwiki@163.com>
2020-02-25 12:35:01 +0800

    lambda理解

--- src/main/java/com/train/lambda/Test.java
+++ src/main/java/com/train/lambda/Test.java
@@ -5,5 +5,66 @@ package com.train.lambda;
  * @Author: kongwiki
  * @Email: kongwiki@163.com
  */
+
+// 推到lambda表达式
 public class Test {
+    //2 静态内部类
+    static class Like2 implements Ilike {
+
+        @Override
+        public void lambda() {
+            System.out.println("i like lambda2");
+        }
+    }
+
+    public static void main(String[] args) {
+        Ilike like = new Like();
+        like.lambda();
+
+        like = new Like2();
+        like.lambda();
+        //3 局部内部类
+        class Like3 implements Ilike {
+
+            @Override
+            public void lambda() {
+                System.out.println("i like lambda3");
+            }
+        }
+
+        like = new Like3();
+        like.lambda();
+
+        //4 匿名内部类
+        like = new Ilike() {
+            @Override
+            public void lambda() {
+                System.out.println("i like lambda4");
+            }
+        };
+        like.lambda();
+
+        // 5 lambda
+        like = () -> {
+            System.out.println("i like lambda5");
+        };
+        like.lambda();
+
+
+
+    }
+}
+
+//定义一个函数式接口
+interface Ilike {
+    void lambda();
+}
+
+//1 实现类
+class Like implements Ilike {
+
+    @Override
+    public void lambda() {
+        System.out.println("i like lambda1");
+    }
 }
--- src/main/java/com/train/lambda/Test2.java
+++ src/main/java/com/train/lambda/Test2.java
@@ -6,4 +6,82 @@ package com.train.lambda;
  * @Email: kongwiki@163.com
  */
 public class Test2 {
+    static class Love2 implements ILove{
+
+        @Override
+        public void love(int a) {
+            System.out.println("hello hello hei 2" + a);
+        }
+    }
+
+    public static void main(String[] args) {
+        // 实现类
+        ILove love = new Love1();
+        love.love(1);
+
+        // 静态内部类
+        love = new Love2();
+        love.love(2);
+
+        // 局部内部类
+        class Love3 implements ILove{
+
+            @Override
+            public void love(int a) {
+                System.out.println("hello hello hei 3" + a);
+            }
+        }
+        love = new Love3();
+        love.love(3);
+
+        // 匿名内部类
+        love = new ILove() {
+            @Override
+            public void love(int a) {
+                System.out.println("hello hello hei 4" + a);
+            }
+        };
+        love.love(4);
+
+        // lambda
+        love = (int a) -> {
+            System.out.println("hello hello hei 5" + a);
+        };
+        love.love(5);
+
+        // 简化一: 去掉参数
+        love = (a) -> {
+            System.out.println("hello hello hei 6" + a);
+        };
+        love.love(6);
+
+        // 简化二： 去括号
+        love = a -> {
+            System.out.println("hello hello hei 7" + a);
+        };
+        love.love(7);
+
+        // 简化三： 去花括号
+        love = a -> System.out.println("hello hello hei 8" + a);
+        // 若是只有一行可以去掉花括号
+        // 若是有多行
+
+
+
+
+
+    }
+
+}
+
+interface ILove{
+    void love(int a);
+}
+
+class Love1 implements ILove{
+
+    @Override
+    public void love(int a) {
+        System.out.println("hello hello hei 1" + a);
+    }
 }
